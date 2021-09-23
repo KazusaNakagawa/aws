cddc7b4d62889120835bb4f3c175bd697ffe7025
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-09-23 10:42:50 +0900

    feat: setting file, const/logging

---
+++ config/const.py
@@ -0,0 +1,15 @@
+# AWS --------------
+CLIENT = 's3'
+BUCKET_NAME = 'testbucket-yyyymmdd'
+TOKYO_REGION = 'ap-northeast-1'
+
+LOGFILE_PATH = 'log/'
+LOG_FILE = 'bucket.log'
+TMP_PATH = 'tmp/'
+
+BUCKET_CREATE = 1
+BUCKET_DELETE = 1
+
+UPLOAD = 1
+DOWNLOAD = 0
+DELETE = 1
