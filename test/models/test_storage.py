6c045f1bd1b4d295b6e8a31f574220e9612995fd
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-09-29 08:49:06 +0900

    feat: create data upload & delete

--- test/models/test_storage.py
+++ test/models/test_storage.py
@@ -11,6 +11,13 @@ from dotenv import load_dotenv
 load_dotenv()
 region = os.environ.get('AWS_REGION')
 
+CONTENT = "INSERT INTO user (name, age, email) VALUES\n" \
+          "('test_name_1', 10, 'sample_1@com'),\n" \
+          "('test_name_2', 20, 'sample_2@com'),\n" \
+          "('test_name_3', 30, 'sample_3@com');\n"
+
+VERSION_1 = False
+
 
 class TestStorage(object):
     storage = None
@@ -18,6 +25,7 @@ class TestStorage(object):
     @classmethod
     def setup_class(cls):
         print('start')
+        cls.test_file_name = 'test.sql'
 
     @classmethod
     def teardown_class(cls):
@@ -31,6 +39,7 @@ class TestStorage(object):
 
     @pytest.fixture
     def _storage(self):
+        """ original fixture """
         return Storage(client=const.CLIENT, bucket_name='testbucket-yyyymmdd', region=region)
 
     def test_create_bucket(self, _storage):
@@ -40,10 +49,10 @@ class TestStorage(object):
         # bucket name duplication avoidance process
         _storage.delete_all_buckets()
 
-        response = _storage.create_bucket()
-        assert len(response['ResponseMetadata']['RequestId']) == 16
-        assert len(response['ResponseMetadata']['HostId']) == 76
-        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+        res = _storage.create_bucket()
+        assert len(res['ResponseMetadata']['RequestId']) == 16
+        assert len(res['ResponseMetadata']['HostId']) == 76
+        assert res['ResponseMetadata']['HTTPStatusCode'] == 200
 
     def test_create_bucket_raise_bucket_already_exists(self, _storage, bucket_name='testbucket'):
         """ If it is already created globally, an exception will be thrown. """
@@ -70,6 +79,30 @@ class TestStorage(object):
             err.value) == 'An error occurred (BucketAlreadyOwnedByYou) when calling the CreateBucket operation: ' \
                           'Your previous request to create the named bucket succeeded and you already own it.'
 
+    def test_upload_data(self, _storage, tmpdir):
+        """ Prepare the data for testing. """
+        test_file = tmpdir.mkdir("sub").join(self.test_file_name)
+        test_file.write(CONTENT)
+        print(tmpdir)
+
+        assert test_file.read() == CONTENT
+        assert len(tmpdir.listdir()) == 1
+        assert os.path.exists(test_file) is True
+
+        _storage.upload_data(bucket_name='testbucket-yyyymmdd',
+                             upload_data=str(test_file))
+
+    def test_delete_data(self, _storage):
+        """ test delete date """
+
+        res = _storage.delete_data(bucket_name='testbucket-yyyymmdd',
+                                   delete_data='test.sql')
+
+        assert len(res['ResponseMetadata']['RequestId']) == 16
+        assert len(res['ResponseMetadata']['HostId']) == 76
+        assert res['ResponseMetadata']['HTTPStatusCode'] == 204
+
+    @pytest.mark.skipif(condition=VERSION_1, reason='skip version1')
     def test_delete_all_buckets(self, _storage):
         """ Test to delete all buckets. """
 
