09b619f57eff6b5033dbb23ab84bd01145fb754e
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-10-01 23:49:28 +0900

    fix: コマンドラインから設定ファイルを読み込むように修正

--- config/const.py
+++ config/const.py
@@ -1,14 +1,51 @@
+import configparser
+import sys
+
+"""
+任意の設定ファイルを読み込んで、定数として格納するファイル
+設定ファイルから読み込む事で、プロクラムを変更しないようにする。
+
+params:
+  sys.argv[1]: sample.ini
+  
+>> sample.ini
+;AWS
+[Storage]
+Client = s3
+BucketName = testbucket-yyyymmdd
+
+[Bucket]
+Create = 1
+Delete = 1
+
+[Management]
+Upload = 1
+Download = 0
+Delete = 1
+TmpPath = tmp/
+
+# Log file
+[Log]
+FilePath = log/
+File = bucket.log
+"""
+
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+
 # AWS --------------
-CLIENT = 's3'
-BUCKET_NAME = 'testbucket-yyyymmdd'
+CLIENT = config['Storage']['Client']
+BUCKET_NAME = config['Storage']['BucketName']
 
-LOGFILE_PATH = 'log/'
-LOG_FILE = 'bucket.log'
-TMP_PATH = 'tmp/'
+BUCKET_CREATE = config['Bucket'].getboolean('Create')
+BUCKET_DELETE = config['Bucket'].getboolean('Delete')
 
-BUCKET_CREATE = 1
-BUCKET_DELETE = 1
+# Management
+TMP_PATH = config['Management']['TmpPath']
+UPLOAD = config['Management'].getboolean('Upload')
+DOWNLOAD = config['Management'].getboolean('Download')
+DELETE = config['Management'].getboolean('Delete')
 
-UPLOAD = 1
-DOWNLOAD = 0
-DELETE = 1
+# Logfile
+LOGFILE_PATH = config['Log']['FilePath']
+LOG_FILE = config['Log']['File']
