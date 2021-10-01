6c3b734b06730441cee12e2b081181a1d0cf6c2a
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-10-01 22:36:09 +0900

    refactor: setting file configparserを使用

--- api/controllers/storage_controller.py
+++ api/controllers/storage_controller.py
@@ -1,9 +1,14 @@
+import configparser
 import os
 
 from dotenv import load_dotenv
 
-from config import const
 from api.models import storage
+
+config = configparser.ConfigParser()
+config.read('config/awsS3.ini')
+
+BUCKET_NAME = config['Storage']['BucketName']
 
 
 def s3_storage_management():
@@ -13,26 +18,28 @@ def s3_storage_management():
     load_dotenv()
     region = os.environ.get('AWS_REGION')
 
-    management_storage = storage.Storage(client=const.CLIENT, bucket_name=const.BUCKET_NAME, region=region)
+    management_storage = storage.Storage(client=config['Storage']['Client'],
+                                         bucket_name=config['Storage']['BucketName'],
+                                         region=region)
 
     data_list = management_storage.get_file_data(base_path='data')
 
-    if const.BUCKET_CREATE:
+    if config['Bucket'].getboolean('Create'):
         management_storage.create_bucket()
 
-    if const.UPLOAD:
+    if config['S3'].getboolean('Upload'):
         for data in data_list:
-            management_storage.upload_data(bucket_name=const.BUCKET_NAME, upload_data=data)
+            management_storage.upload_data(bucket_name=BUCKET_NAME, upload_data=data)
 
-    if const.DOWNLOAD:
+    if config['S3'].getboolean('Download'):
         for data in data_list:
             management_storage.download_data(download_data=data)
 
-    if const.DELETE:
+    if config['S3'].getboolean('Delete'):
         for data in data_list:
-            management_storage.delete_data(bucket_name=const.BUCKET_NAME, delete_data=data)
+            management_storage.delete_data(bucket_name=BUCKET_NAME, delete_data=data)
 
-    if const.BUCKET_DELETE:
+    if config['Bucket'].getboolean('Delete'):
         management_storage.delete_all_buckets()
 
     # 最終的に存在している Bucketを確認する
